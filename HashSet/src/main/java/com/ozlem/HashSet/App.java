cd89c975a58cfd78c29bb1c22b3c06b8f721f69a
Özlem Pala <peline_93@hotmail.com>
2019-01-21 10:15:57 +0300

    hashmap duplicate value

--- HashSet/src/main/java/com/ozlem/HashSet/App.java
+++ HashSet/src/main/java/com/ozlem/HashSet/App.java
@@ -1,5 +1,6 @@
 package com.ozlem.HashSet;
-
+import java.util.HashMap;
+import java.util.Set;
 /**
  * Hello world!
  *
@@ -8,6 +9,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	HashMap<String, String> hm = new HashMap<String, String>();
+        hm.put("20", "Banana");
+        hm.put("40", "Apple");
+        hm.put("20", "Banana");
+        printMap(hm);
+    }
+
+    public static void printMap(HashMap<String, String> map){
+        
+        Set<String> keys = map.keySet();
+        for(String p:keys){
+            System.out.println(p+"==>"+map.get(p));
+        }
     }
 }
